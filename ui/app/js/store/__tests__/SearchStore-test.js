8e8ed5458a98aea5ea31e68fbd1cf70ac0e2c58c
Michal Kowol <michal.kowol@gmail.com>
2015-11-12 15:46:44 +0100

    NT. Fixed js tests.

--- ui/app/js/store/__tests__/SearchStore-test.js
+++ ui/app/js/store/__tests__/SearchStore-test.js
@@ -8,14 +8,26 @@ describe('SearchStore', function () {
     // given
     const SearchActions = require('./../../action/SearchActions');
     const SearchStore = require('../SearchStore');
-    const movies = [{name: 'test1', genre: 'SciFi', forChildren: false}];
+    const movies = [{title: 'test1', genre: 'SciFi', forChildren: false}, {title: 'test2', genre: 'SciFi', forChildren: false}];
+    // when no movies are loaded
+    expect(SearchStore.filteredMovies()).toEqual([]);
+    SearchActions.changeSearchText('test');
+    expect(SearchStore.filteredMovies()).toEqual([]);
+    SearchActions.changeSearchText('');
     // when
     SearchActions.loadMovies(movies);
+    // then
+    expect(SearchStore.filteredMovies()).toEqual(movies);
+    // when
     SearchActions.changeSearchText('test');
     // then
-    expect(SearchStore.filteredMovies()).toEqual([{name: 'test1', genre: 'SciFi', forChildren: false}]);
+    expect(SearchStore.filteredMovies()).toEqual(movies);
     // when
-    SearchActions.changeSearchText('spec');
+    SearchActions.changeSearchText('test1');
+    // then
+    expect(SearchStore.filteredMovies()).toEqual([{title: 'test1', genre: 'SciFi', forChildren: false}]);
+    // when
+    SearchActions.changeSearchText('test3');
     // then
     expect(SearchStore.filteredMovies()).toEqual([]);
   });
